04d1b7d0a25bde9f08f69d52ef8aa6e381bd2574
mudasirahanger <mudasir.ahanger@digitalindia.gov.in>
2026-01-01 17:00:08 +0530

    removd un used migration

--- amigos-backend/database/factories/UserFactory.php
+++ amigos-backend/database/factories/UserFactory.php
@@ -25,10 +25,15 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'mobile_no' => fake()->unique()->phoneNumber(),
+            'latitude' => fake()->latitude(),
+            'longitude' => fake()->longitude(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'address' => fake()->address(),
             'remember_token' => Str::random(10),
+            'timestamps' => now(),
         ];
     }
 
