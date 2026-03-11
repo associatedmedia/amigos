dcbaa25f97015fc1e9b63f075259ef3debb00aa5
mudasirahanger <mudasir.ahanger@digitalindia.gov.in>
2026-03-11 11:11:33 +0530

    image upload fix

--- amigos-backend/app/Http/Controllers/webadmin/ProductController.php
+++ amigos-backend/app/Http/Controllers/webadmin/ProductController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Storage;
+
 
 class ProductController extends Controller
 {
